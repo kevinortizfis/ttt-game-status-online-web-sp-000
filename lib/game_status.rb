7bc5cc2b2b7b0957351d09a27be11edd5c48418f
Kevin Ortiz <kevin.ortiz@flatironschool.com>
2020-07-19 01:55:37 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,45 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS =
+  [[0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]]
+
+def won?(board)
+if board.all?(" ")
+  false
+elsif !board.all?(" ")
+      WIN_COMBINATIONS.each do |combination|
+        position_1 = combination[0]
+        position_2 = combination[1]
+        position_3 = combination[2]
+        combination.each do |position|
+          if (board[position_1] == board[position_2] && board[position_2] == board[position_3]) && position_taken?(board,position_1)
+            return combination
+          elsif board.none?(" ")
+            return false
+          end
+        end
+      end
+    end
+  end
+
+def full?(board)
+  if board.none?(" ")
+    return true
+  else
+    return false
+  end
+end
+
+def draw?(board)
+  if won?(board) == false && full?(board)
+    return true
+  end
+end
